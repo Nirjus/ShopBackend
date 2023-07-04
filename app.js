1531e32c34a0eb5740e248c1e409c681fcd957bd
Nirjus Karmakar <128937557+Nirjus@users.noreply.github.com>
2023-07-04 17:54:22 +0530

    in app jsh

--- app.js
+++ app.js
@@ -4,12 +4,11 @@ const app = express();
 const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
 const cors = require("cors");
-const path = require("path");
 
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-    origin:"https://shop-app-s8q6.vercel.app",
+    origin:["https://shop-app-s8q6.vercel.app",],
     credentials:true,
 }));
 
